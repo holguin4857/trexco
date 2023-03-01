e0a8858db28873386f52a3d5d77ce005134bfb39
holguin4857 <omar@trexco.net>
2023-03-01 15:41:40 +0100

    footer-new

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '@/styles/navbar.css'
 import '@/styles/layout.css'
-import '@/styles/Home.module.css'
 import '@/styles/footer.css'
 import { Poppins } from 'next/font/google'
 import Navbar from '@/components/Navbar'
--- pages/privacy.js
+++ pages/privacy.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 
-export default function How() {
+export default function Privacy() {
   return (
     <>
       <Head>
